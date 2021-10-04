4b6ebf407a8346a318cc6eb6966e1d2d2d10923f
ArturoSbr <soberonarturo@gmail.com>
2021-10-04 09:46:27 -0500

    DMS to numeric parsing

--- proyectos/RespuestaDesastre/tarea1/cod/etl.R
+++ proyectos/RespuestaDesastre/tarea1/cod/etl.R
@@ -11,24 +11,24 @@ library(tidyr)
 library(sp)
 
 # Cargar datos
-df <- lapply(excel_sheets('./dat/refugios_nayarit.xlsx'), read_xlsx,
-             path='./dat/refugios_nayarit.xlsx', col_names=FALSE, skip=6) %>% bind_rows()
+path <- '../dat/refugios_nayarit.xlsx'
+df <- lapply(excel_sheets(path), read_xlsx, path=path, col_names=FALSE, skip=6) %>% bind_rows()
 
 # Nombre de columnas
 names(df) <-c('id','refugio','municipio','direccion','tipo','servicios','capacidad','lat','long',
               'alt','responsable','tel')
 
-# Partir en alfa (en vez de gsub)
+# Quitar NAs sobre `id`, `lat` y `long`
+df <- df %>% filter(!(is.na(id) | is.na(lat) | is.na(long)))
+# Quitar espacios en `lat` y `long`
+df <- df %>% mutate(lat=gsub(' ','',lat), long=gsub(' ','',long))
+
+# Partir en caracteres no-numéricos (en vez de gsub)
 t <- df %>%
-  select(c(lat,long)) %>% 
+  select(c(id,lat,long)) %>% 
   separate(lat, into=paste0('lat', 1:4), sep='[^0-9]', remove=FALSE) %>% 
   separate(long, into=paste0('long', 1:4), sep='[^0-9]', remove=FALSE) %>% 
-  mutate(lat=paste0(lat1,'d',lat2,'m',lat3,'s',lat4)) %>% 
-  mutate(long=paste0(long1,'d',long2,'m',long3,'s',long4)) %>% 
-  select(c(lat,long)) %>% 
-  drop_na(c(lat, long))
+  mutate(lat=paste0(lat1,'d',lat2,'m',lat3,'.',lat4,'s')) %>% 
+  mutate(long=paste0(long1,'d',long2,'m',long3,'.',long4,'s'))
 
-# STR a DMS a NUM
-coords <- c("22d29m56s06", "105d21m37s27")
-char2dms(from=coords, chd='d', chm='m', chs='s') %>% as.numeric()
-
+char2dms(from=c(t$lat, t$long), chd='d', chm='m', chs='s') %>% as.numeric()
